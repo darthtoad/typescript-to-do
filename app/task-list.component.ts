9fa082660a05baf2ae161361ff7a9253f77f7f07
Sam Gespass <darth.toad@gmail.com>
2018-02-19 15:21:26 -0800

    finish angular tutorial through Wednesday

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,8 +4,18 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
+  <select (change)="onChange($event.target.value)"  id="select-task">
+    <option value="allTasks">All Tasks</option>
+    <option value="completedTasks">Completed Tasks</option>
+    <option value="incompleteTasks"  selected="selected">Incomplete Tasks</option>
+  </select>
+
   <ul>
-    <li *ngFor="let currentTask of childTaskList"><h3>{{currentTask.name}}</h3>{{currentTask.description}}<br>Priority: {{currentTask.priority}}<br>   <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>      <button (click)="isComplete(currentTask)">Check Completeness</button></li>
+    <li *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness"><h3>{{currentTask.name}}</h3>{{currentTask.description}}<br>Priority: {{currentTask.priority}}<br>
+    Complete?
+    <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+    <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/><br>
+    <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
   </ul>
   `
 })
@@ -16,12 +26,10 @@ export class TaskListComponent {
 
   @Output() clickSender = new EventEmitter();
 
-  isComplete(thingy: Task) {
-    if(thingy.done === true) {
-      alert("You have finished your task. Good work!");
-    } else {
-      alert("YOU HAVE NOT COMPLETED YOUR TASK, YOU IMBECILE!");
-    }
+  filterByCompleteness: string = "incompleteTasks";
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
   }
 
   priorityColor(currentTask){
@@ -36,5 +44,9 @@ export class TaskListComponent {
 
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
+  }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+   clickedTask.done = setCompleteness;
   }
 }
