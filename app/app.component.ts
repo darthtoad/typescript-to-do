ceb3df9689518d2fcf524e39038bf3308419e9fa
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-19 09:37:53 -0800

    complete lesson on event bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}-{{day}}-{{year}}</h1>
     <h2>{{currentFocus}}</h2>
     <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+      <li *ngFor="let currentTask of tasks">{{currentTask.description}}   <button (click)="editTask()">Edit!</button>      <button (click)="isComplete(currentTask)">Check Completeness</button></li>
     </ul>
   </div>
   `
@@ -21,6 +21,19 @@ export class AppComponent {
   year: number = this.now.getFullYear();
   luckyNumber: number = Math.floor(this.now.getTime() * Math.random() / 420);
   tasks: Task[] = [new Task("Go to ReactNative meeting", "Meet new people, learn more about ReactNative", 3), new Task("Poop my pants", "It's fun", 4), new Task("I am Tosk", "I am Tosk", 9)];
+
+
+  editTask() {
+    alert("Holy shit you're going to edit a task!");
+  }
+
+  isComplete(thingy: Task) {
+    if(thingy.done === true) {
+      alert("You have finished your task. Good work!");
+    } else {
+      alert("YOU HAVE NOT COMPLETED YOUR TASK, YOU IMBECILE!");
+    }
+  }
 }
 
 export class Task {
